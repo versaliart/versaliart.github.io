3bd7cb91d320a182659b46783de240f3db55852d
versaliart <161543320+versaliart@users.noreply.github.com>
2025-02-01 16:41:56 -0800

    Update Cargo JSS.js

--- docs/Cargo JSS.js
+++ docs/Cargo JSS.js
@@ -43,3 +43,16 @@ document.addEventListener("DOMContentLoaded", function () {
         eyeContainer.setAttribute("transform", `scale(${scaleFactor})`);
     }
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute("href"));
+      if (target) {
+        target.scrollIntoView({
+          behavior: "smooth"
+        });
+      }
+    });
+  });
+  
