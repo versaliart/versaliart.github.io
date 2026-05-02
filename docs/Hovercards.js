5b0af2fd87a0484ec8737c9b741eed1e084dc11c
versaliart <161543320+versaliart@users.noreply.github.com>
2026-05-02 10:07:55 -0700

    Update Hovercards.js

--- docs/Hovercards.js
+++ docs/Hovercards.js
@@ -72,18 +72,18 @@ groups.forEach(({ target, fallbackElements }) => {
         <svg viewBox="0 0 100 100" preserveAspectRatio="none">
           <rect
             class="mm-glow-line"
-            x="1"
-            y="1"
-            width="98"
-            height="98"
+            x="0.5"
+            y="0.5"
+            width="99"
+            height="99"
             pathLength="100"
           />
           <rect
             class="mm-glow-blur"
-            x="1"
-            y="1"
-            width="98"
-            height="98"
+            x="0.5"
+            y="0.5"
+            width="99"
+            height="99"
             pathLength="100"
           />
         </svg>
